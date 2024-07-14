600e8f2d1d920950341d8b0c472d53f731418308
Ramailhamisamda <ramasamda@gmail.com>
2024-07-14 22:53:34 +0700

    next commit

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
 const port = 3000
+app.use(router)
 
-router.get('/toko', (req, res) => {
+
+router.get('/', (req, res) => {
   res.send('App is running..');
+  // console.log("test")
+  // res.status(200).json({
+  //   message : "test"
+  // })
 });
 
-router.listening(port,()=>{
+app.listen(port,()=>{
   console.log(`app is listening on port ${port}`)
 })
+router.get('/tokoo',(req,res)=>{
+  // res.render('index.ejs')
+  res.render('indexx.ejs')
+})
+
 app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
